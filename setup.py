5729223f9e7427f9063d2d321b3e4ea13980d532
Mikołaj Badyl <mikolajbadyl0@gmail.com>
2025-08-11 13:28:31 +0200

    feat: prepare package for PyPI publication

--- setup.py
+++ setup.py
@@ -23,7 +23,9 @@ setup(
         ],
     },
     author="hawier-dev",
-    author_email="",
+    author_email="mikolajbadyl0@gmail.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/hawier-dev/pmpt-cli",
     classifiers=[
         "Development Status :: 4 - Beta",
